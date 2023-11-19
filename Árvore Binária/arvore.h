4d129cf2eab8ce38be2f20cf0e3f380ee7372786
Gabriel Oliveira <gomm.gabreil@gmail.com>
2023-11-19 15:02:56 -0300

    Definição das funções

---
+++ Árvore Binária/arvore.h
@@ -0,0 +1,20 @@
+#ifndef ARVORE_H
+#define ARVORE_H
+
+typedef struct TreeNode {
+    int key;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} TreeNode;
+
+TreeNode* criarNo(int chave);
+TreeNode* inserir(TreeNode* raiz, int chave);
+TreeNode* buscar(TreeNode* raiz, int chave);
+TreeNode* remover(TreeNode* raiz, int chave);
+void percorrerPreOrdem(TreeNode* raiz);
+void percorrerEmOrdem(TreeNode* raiz);
+void percorrerPosOrdem(TreeNode* raiz);
+TreeNode* minimo(TreeNode* raiz);
+TreeNode* maximo(TreeNode* raiz);
+
+#endif 
---
+++ Árvore Binária/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "arvore.h"
+
+int main() {
+    
+    printf("Teste de Compilação");
+    return 0;
+}
